1cc34a89d664c3cf849b70876f60197939b4a1cf
piopratama <piopratamai@yahoo.com>
2019-03-26 15:17:53 +0800

    pio

--- mainMenu.php
+++ mainMenu.php
@@ -54,11 +54,7 @@ else
 			<div class="row">
 				<div class="col-md-4"></div>
 				<div class="col-md-4">
-<<<<<<< HEAD
-					<a href="directPayTable.php"><button type="button" class="btn btn-default" id="directpayMenu">Direct Pay</button></a>
-=======
 					<a href="directPay.php"><button type="button" class="btn btn-default" id="directpayMenu">Transaksi</button></a>
->>>>>>> b42e5b08aaa38bea2e0c8dd5115fa60b499bab8e
 				</div>
 				<div class="col-md-4"></div>
 			</div>
