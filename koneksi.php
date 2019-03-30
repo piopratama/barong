6c2ee81825e618dd676afe88dd587eb2179a9e69
piopratama <piopratamai@yahoo.com>
2019-03-31 07:53:43 +0800

    pio

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,4 @@
 <?php
-	/*$servername = "sql130.main-hosting.eu";
-	$username = "u610112734_baron";
-	$password = "barong_123*";
-	$dbname = "u610112734_baron";*/
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
