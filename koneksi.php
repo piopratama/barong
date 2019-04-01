af992ecbccdf552f8ba675d22be351753bb761b3
piopratama <piopratamai@yahoo.com>
2019-04-01 22:06:14 +0800

    pio - add transaction in case something happened during insert transaction

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "deli_shop";
+	$dbname = "barong";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
